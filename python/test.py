d266935f25d79bae3b475780e303fd2872999827
rokamu <r.okamura.061@ms.saitama-u.ac.jp>
2021-11-14 18:29:48 +0900

    add_DAG_TGFF

--- python/test.py
+++ python/test.py
@@ -1,5 +1,5 @@
-from DAG_base import DAG_base
+from DAG_TGFF_load import DAG_TGFF
 
-test_dag = DAG_base("original_10_0")
+test_dag = DAG_TGFF("original_10_0")
 test_dag.read_file_tgff()
 test_dag.check()

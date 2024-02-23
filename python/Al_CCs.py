3d9527e6dd3bc342ac9d491cad435346d0f732b4
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2024-02-23 21:50:15 +0900

    refactor: proposed method (WiP)
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- python/Al_CCs.py
+++ python/Al_CCs.py
@@ -1,7 +1,7 @@
 from numpy import argmax
 from numpy import argmin
 from math import ceil
-from DAGs.DAG_base import Node
+from .DAGs.DAG_base import Node
 from enum import Enum
 
 class Method(Enum):
@@ -10,24 +10,24 @@ class Method(Enum):
     Dec_method = 2
     Basic = 3
 
-def Al_CCs(dags: [Node], method=Method.Wait_suff):
+def Al_CCs(dags: list[Node], method=Method.Wait_suff):
     # initialize
-    q = []
-    e = []
-    vacant_core = [ 16 for i in range(5) ]
-    cores = [ [ i for i in range(16) ] for i in range(5) ]
+    wait_queue: list[Node] = []
+    executing_nodes: list[Node] = []
+    vacant_core: list[int] = [ 16 for i in range(5) ]
+    cores: list[list[int]] = [ [ i for i in range(16) ] for i in range(5) ]
 
-    time = 0
+    time: int = 0
 
-    dags[0].time = 0
+    dags[0].start_time = 0
     dags[0].st_flag = True
-    q.append(dags[0])
+    wait_queue.append(dags[0])
 
     while True:
         
         # allocation
-        while len(q) > 0:
-            v_h = q.pop(0)
+        while len(wait_queue) > 0:
+            v_h = wait_queue.pop(0)
             #print([99 if i < v_h.n else i for i in vacant_core])
             if method is not Method.Basic:
                 cc_idx = argmin([99 if core_num < v_h.n else core_num for core_num in vacant_core])
@@ -35,38 +35,38 @@ def Al_CCs(dags: [Node], method=Method.Wait_suff):
                 cc_idx = argmin([99 if core_num != 16 else core_num for core_num in vacant_core])
             if vacant_core[cc_idx] >= v_h.n:
                 v_h.cc_idx = cc_idx
-                v_h.time = time
+                v_h.start_time = time
                 vacant_core[cc_idx] -= v_h.n
                 for i in range(v_h.n):
                     v_h.core_idx.append(cores[cc_idx].pop(0))
-                e.append(v_h)
+                executing_nodes.append(v_h)
             else:
                 if method is Method.Wait_suff or method is Method.Basic:
-                    q.insert(0, v_h)
+                    wait_queue.insert(0, v_h)
                     break
                 elif method is Method.Al_avail:
                     cc_idx = argmax(vacant_core)
                     if vacant_core[cc_idx] != 0:
                         v_h.n = vacant_core[cc_idx]
                         v_h.cc_idx = cc_idx
-                        v_h.time = time
+                        v_h.start_time = time
                         vacant_core[cc_idx] -= v_h.n
                         for i in range(v_h.n):
                             v_h.core_idx.append(cores[cc_idx].pop(0))
-                        e.append(v_h)
+                        executing_nodes.append(v_h)
                     else:
-                        q.insert(0, v_h)
+                        wait_queue.insert(0, v_h)
                         break
                 elif method is Method.Dec_method:
                     cc_idx = argmax(vacant_core)
                     al_avail_flag = True
                     if vacant_core[cc_idx] != 0:
                         core_num = vacant_core[cc_idx]
-                        tmp_time = time + v_h.sc_n(core_num)
+                        tmp_time = time + v_h.amdahl_exe_time_with_n_core(core_num)
 
-                        for node in sorted([node for node in e if node.cc_idx == cc_idx], key=lambda u: u.time+u.sc()):
+                        for node in sorted([node for node in executing_nodes if node.cc_idx == cc_idx], key=lambda u: u.start_time+u.amdahl_exec_time()):
                             core_num += node.n
-                            if tmp_time > node.time+node.sc()+v_h.sc_n(core_num):
+                            if tmp_time > node.start_time+node.amdahl_exec_time()+v_h.amdahl_exe_time_with_n_core(core_num):
                                 al_avail_flag = False
                     else:
                         al_avail_flag = False
@@ -74,45 +74,56 @@ def Al_CCs(dags: [Node], method=Method.Wait_suff):
                     if al_avail_flag is True:
                         v_h.n = vacant_core[cc_idx]
                         v_h.cc_idx = cc_idx
-                        v_h.time = time
+                        v_h.start_time = time
                         vacant_core[cc_idx] -= v_h.n
                         for i in range(v_h.n):
                             v_h.core_idx.append(cores[cc_idx].pop(0))
-                        e.append(v_h)
+                        executing_nodes.append(v_h)
                     else:
-                        q.insert(0, v_h)
+                        wait_queue.insert(0, v_h)
                         break
 
                 else:
-                    print("invalid-method")
+                    print('invalid-method')
                     exit()
                 
 
         # simulation
-        # find earliest
-        e = sorted(e, key=lambda node: node.time+node.sc())
-        while(len(e) != 0):
-            earliest = e.pop(0)
-
-            vacant_core[earliest.cc_idx] += earliest.n
-            cores[earliest.cc_idx].extend(earliest.core_idx)
-            time = earliest.time + earliest.sc()
+        # 早く終わる順に並び変え
+        executing_nodes = sorted(executing_nodes, key=lambda node: node.start_time+node.amdahl_exec_time())
+        while True:
+            # 最も早く終わるものを取得し、終了をフラグ
+            earliest = executing_nodes.pop(0)
             earliest.fn_flag = True
 
-            if len(e) == 0 or e[0].time + e[0].sc() != time:
+            # コア状況を更新（終了した分を戻す）
+            vacant_core[earliest.cc_idx] += earliest.n
+            cores[earliest.cc_idx].extend(earliest.core_idx)
+
+            # 時刻の更新
+            time = earliest.start_time + earliest.amdahl_exec_time()
+
+            # 同じ時刻に完了するノードがあったら続行
+            if len(executing_nodes) == 0 or executing_nodes[0].start_time + executing_nodes[0].amdahl_exec_time() != time:
                 break
+
+        # 開始しておらず全ての前任が終わったノードを検出
+        new_entry: list[Node] = []
+        for node in dags:
+            # 開始しておらず
+            if node.st_flag is True:
+                continue
+
+            # 前任
+            pre_nodes = [dags[p] for p in node.pre]
+            # 全ての前任が終わっていたら
+            if all([n.fn_flag for n in pre_nodes]):
+                node.st_flag = True
+                new_entry.append(node)
+
+        # 待機キューに追加
+        wait_queue.extend(sorted(new_entry, key=lambda u: u.p, reverse=True))
+
+        # 末尾ノードが実行完了したらループ終了
         if earliest.snk is True:
             break
-
-        new_entry = []
-        for dag in dags:
-            flag = True
-            if dag.st_flag is True:
-                flag = False
-            for p in dag.pre:
-                if dags[p].fn_flag is not True:
-                    flag = False
-            if flag is True:
-                dag.st_flag = True
-                new_entry.append(dag)
-        q.extend(sorted(new_entry, key=lambda u: u.p, reverse=True))
